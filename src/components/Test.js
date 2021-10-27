46edad7d75ac3dc0d6fbf115144501c80e5d3050
Edith M <edith.mejia@gmail.com>
2021-10-27 07:50:25 -0500

    fixes

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,16 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useFeaturedBanners } from '../utils/hooks/useFeaturedBanners';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
-import  styles from '../styles/FeaturedContent.module.css'
-// import Swiper core and required modules
-import SwiperCore, {
-    Navigation
-  } from 'swiper';
-  
-// install Swiper modules
-SwiperCore.use([Navigation]);
+
 
 
 const Test = () => {
